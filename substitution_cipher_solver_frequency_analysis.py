738c0163d068ff946bd32d96f3a2fdf3352bdbff
spooky-book <nomonacookie@gmail.com>
2019-07-11 20:46:11 +1000

    frequency analysis of substitution cipher

---
+++ substitution_cipher_solver_frequency_analysis.py
@@ -0,0 +1,115 @@
+import re, random
+
+from ngram_class import nGramInfo
+
+#substitution cipher
+
+def main():
+	ciphertext = input("Enter the message: ")
+	ciphertext = ciphertext.lower()
+	
+	ciphertext_only_letters = re.sub("[^a-z]+", "", ciphertext)
+
+	#print(ciphertext_only_letters)
+
+	alphabet = list("abcdefghijklmnopqrstuvwxyz")
+	#print(alphabet)
+	#randomly shuffles the alphabet
+	random.shuffle(alphabet)
+	#print(alphabet)
+	starter_key = "".join(alphabet)
+	print(starter_key)
+
+	best_keys = calculate_best_key(ciphertext_only_letters, starter_key, ciphertext)
+	best_keys = sorted(best_keys)
+	print(best_keys)
+
+def calculate_best_key(ciphertext_only_letters, starter_key, ciphertext):
+	
+	#creates object to calculate fitness
+	calculator = create_nGramInfo_class()
+
+	#creates a possible plaintext with the starter key
+	plaintext = substitution(ciphertext_only_letters, starter_key)
+	high_score = calculator.calculate_fitness_score(plaintext)
+	best_key = starter_key
+
+	highest = [[high_score, best_key]]
+	print(high_score)
+	
+	new_key = starter_key
+
+	seen = set()
+
+	iteration = 0
+	while 1:
+		random.shuffle(list(new_key))
+		new_key = "".join(new_key)
+		
+		iteration += 1
+		i = 0
+		
+		while i < 10000:
+			
+			num1 = num2 = 0
+			while num1 == num2:
+				num1 = random.randint(0, 25)
+				num2 = random.randint(0, 25)
+
+			new = list(new_key)
+			new[num1], new[num2] = new[num2], new[num1]
+
+			new_key = "".join(new)
+			#print(new_key)
+
+			plaintext = substitution(ciphertext_only_letters, new_key)
+			new_score = calculator.calculate_fitness_score(plaintext)
+
+			seen.add(new_key)
+
+			if new_score > high_score:
+				high_score = new_score
+				best_key = new_key
+				highest.append([high_score, best_key])
+				#print("yeet")
+			
+			i += 1
+
+		print("Current highest score is", high_score, "on iteration", iteration)
+		print("This decodes to", substitution(ciphertext, best_key))
+
+	return highest
+
+def create_nGramInfo_class():
+	print("Enter '1' for monograms")
+	print("Enter '2' for bigrams")
+	print("Enter '3' for trigrams")
+	print("Enter '4' for quadgrams")
+	print("Note: quadgrams can only do analysis on messages >= 4 characters\ntrigrams for >= 3 and so on \n(if you need a program to help decipher a < 4 letter caesar cipher RIP)")
+	mode = input("Enter Mode: ")
+	mode = mode.strip().lower()
+
+	if mode == '1':
+		file_name = "ngrams/english_monograms.txt"
+	elif mode == '2':
+		file_name = "ngrams/english_bigrams.txt"
+	elif mode == '3':
+		file_name = "ngrams/english_trigrams.txt"
+	elif mode == '4':
+		file_name = "ngrams/english_quadgrams.txt"
+	else: 
+		print("Make sure input is correct")
+		exit()
+
+	return nGramInfo(file_name)
+
+
+def substitution(ciphertext_only_letters, key):
+	plaintext = ""
+	for letter in ciphertext_only_letters:
+		plaintext += key[ord(letter) - ord("a")]
+
+	return plaintext
+
+if __name__ == "__main__":
+	main()
